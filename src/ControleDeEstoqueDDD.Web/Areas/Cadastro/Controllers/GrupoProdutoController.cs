087e3d8bd10f034c3ed82e7855f0fd944471f16e
BuildTools <unconfigured@null.spigotmc.org>
2022-02-08 19:03:04 -0300

    first commit

---
+++ src/ControleDeEstoqueDDD.Web/Areas/Cadastro/Controllers/GrupoProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ControleDeEstoqueDDD.Web.Models;
+
+namespace ControleDeEstoqueDDD.Web.Controllers
+{
+    [Area("Cadastro")]
+    public class GrupoProdutoController : Controller
+    {
+        private readonly ILogger<GrupoProdutoController> _logger;
+
+        public GrupoProdutoController(ILogger<GrupoProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/ControleDeEstoqueDDD.Web/Areas/Cadastro/Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ControleDeEstoqueDDD.Web.Models;
+
+namespace ControleDeEstoqueDDD.Web.Controllers
+{
+    [Area("Cadastro")]
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
